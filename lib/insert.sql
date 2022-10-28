8e151247bb7812b5ceb616caaa289fb1fcded46a
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-10-28 14:11:31 +0300

    Complete Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate",6,"M","Black","Sweet",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",9,"M","Black and Brown","Moody",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",5,"F","White","Sweet",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",9,"M","Brown","Protective",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",4,"F","Black and White","Sweet",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",7,"F","Black and White","Sad",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",4,"F","Black and White","Sweet",1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,7,"F","Black and White","Sulky",1);
